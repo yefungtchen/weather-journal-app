f3e25a4cc9b575ed0eb5f3f5dca9a7341911fe20
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-04-27 11:47:40 +0200

    Added a .gitignore

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const express = require('express');
 // ** Start up an instance of app
 const app = express();
 
-
 // Start up an instance of app
 
 /* Middleware*/
@@ -23,6 +22,7 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 // **
 const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
