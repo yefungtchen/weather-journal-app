d0db9baf1617edffcd4a8c8716a9f6f19cea8f7c
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-05-05 09:30:35 +0200

    Updated server configs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};node
+projectData = {};
 
 // Require Express to run server and routes
 // ** Express to run server and routes
@@ -14,17 +14,30 @@ const app = express();
 // ** Made package in code available
 var bodyParser = require('body-parser');
 
-//Here we are configuring express to use body-parser as middle-ware.
-
+// Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-// **
+// CORS is a node. js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 const cors = require('cors');
+
+// Here we are configuring express to use cors 
 app.use(cors());
 
 // Initialize the main project folder
+// This line of code connects our server-side code (the code in the server.js file) to our client-side code (the browser code written in the files housed in the website folder).
+
+
 app.use(express.static('website'));
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+};
+
