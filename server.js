fa5bc8b63328f78c6a35dc9d3209fbbcdd027ad6
Fungieren <ye.fung.tchen@klqc.de>
2020-05-11 15:17:33 +0200

    Adding a Async POST

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ let projectData = {};
 
 // Require Express to run server and routes
 // ** Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 // ** Made package in code available
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 // Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,26 +18,32 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 // CORS is a node. js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-const cors = require('cors');
+const cors = require("cors");
 
-// Here we are configuring express to use cors 
+// Here we are configuring express to use cors
 app.use(cors());
 
 // Initialize the main project folder
 // This line of code connects our server-side code (the code in the server.js file) to our client-side code (the browser code written in the files housed in the website folder).
-app.use(express.static('website'));
-
-// Setting up the Post Route
-app.post('/', function (req, res) {
-    res.send('POST received')
-  })
+app.use(express.static("website"));
 
 // Setting up the Get Route
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-    res.send('Hello world');
-  })
+app.get("/all", function (req, res) {
+  res.send(JSON.stringify(projectData));
+  res.send("Hello world");
+  console.log(projectData);
+});
 
+// Setting up the Post Route
+app.post("/addMovie", function (req, res) {
+  projectData.temp = req.body.temp;
+  projectData.userEntry = req.body.userEntry;
+  projectData.date = req.body.date;
+  console.log(req.body);
+  res.send("POST received, ok ok ok");
+  res.end();
+});
 
 // Setup Server
 const port = 8000;
@@ -45,7 +51,6 @@ const port = 8000;
 const server = app.listen(port, listening);
 
 function listening() {
-    console.log("server running");
-    console.log(`running on localhost: ${port}`);
-};
-
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,37 @@
 /* Global Variables */
+const owmURL = "api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "fa42b0d875248b9ed2383b52c62b0251";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Async POST Function Tutorial used: FEND Udacity Lesson 4 Asynchronous JavaScript
+const postData = async (url = "", data = {}) => {
+  // console.log(data)
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+// postData("/addMovie", { movie: " the matrix", score: 5 });
+
+// Async GET
+
+// Updating the UI
+
+// Click Event "Generate"
